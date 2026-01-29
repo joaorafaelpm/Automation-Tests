a610ee19c93a5519dbdf729701a3b1c27533b73b
João Rafael Pendezza Medeiros <joaorafaelpmed@gmail.com>
2026-01-29 19:04:18 -0300

    Fazendo selectors mais sofisticados

---
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe("test contact us form via Automation Test Store", () => {
+  it("should be able to submit a successful submission via contact us form", () => {
+    cy.visit("https://www.automationteststore.com/");
+    cy.get("a[href$='contact']",).click();
+    // A mesma forma  de fazer  o que foi feito acima,  porém usando xpath
+    // cy.xpath("//a[contains(@href, 'contact')]").click();
+    cy.origin('https://automationteststore.com', () => {
+        cy.get("#ContactUsFrm_first_name").type("Roberto");
+        cy.get("#ContactUsFrm_email").type("roberto.fezzbear@example.com");
+        cy.get("#ContactUsFrm_enquiry").type("Hello, this is a test message for the contact us form.",);
+        cy.get("button[title=Submit]").click();
+    });
+    
+  });
+
+});
---
+++ cypress/e2e/webdriver-uni/selector-examples.js
@@ -0,0 +1,29 @@
+///<reference types="cypress" />
+
+describe("Selector Examples on WebDriverUni", () => {
+  it("Examples of  selectors via WebDriverUni", () => {
+    cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+
+    // Pelo nome da tag
+    cy.get("input");
+
+    // Pelos atributos de dentro da tag
+    cy.get("input[name='first_name']");
+
+    // Pelo ID
+    cy.get("#contact_me");
+
+    // Pela classe
+    cy.get(".feedback-input")
+
+    // Por várias classes
+    cy.get("[class='navbar navbar-inverse navbar-fixed-top']");
+
+    // Por atributos diferentes
+    cy.get("input[name='email'][placegholder='Email Address']")
+
+    // Pelo xpath (necessita do plugin cypress-xpath)
+    cy.xpath("//input[@name='first_name']");
+})
+
+});
